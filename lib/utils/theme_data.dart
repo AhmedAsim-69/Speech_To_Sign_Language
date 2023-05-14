8bf7d96b467376ddb95a8de9bb75676c4defb2a7
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-14 18:51:01 +0500

    Added Support for Light/Dark mode + Defined Theme

--- lib/utils/theme_data.dart
+++ lib/utils/theme_data.dart
@@ -1,26 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:stsl/services/user_simple_preferences.dart';
 
 class ThemeNotifier with ChangeNotifier {
-  final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.black,
-    brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    // accentColor: Colors.white,
-    // accentIconTheme: IconThemeData(color: Colors.black),
-    dividerColor: Colors.black12,
+  final lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      primary: const Color(0xFF42a79d),
+      secondary: const Color(0xFF42a79d),
+    ),
+    fontFamily: "GoogleSans",
+    primaryTextTheme: GoogleFonts.acmeTextTheme(),
+    brightness: Brightness.light,
+    backgroundColor: const Color(0xFFF5F5F5),
+    scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      color: Color(0xFF42a79d),
+      foregroundColor: Colors.black,
+      iconTheme: IconThemeData(color: Colors.black),
+    ),
+    bottomAppBarColor: Colors.white,
+    iconTheme: const IconThemeData(color: Colors.black),
+    textTheme: const TextTheme(
+      headline1: TextStyle(
+        color: Colors.black,
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+      headline2: TextStyle(
+        color: Colors.black,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      headline3: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyText1: TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+      ),
+      bodyText2: TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          const Color(0xFF42a79d),
+        ),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: TextStyle(
+        color: Colors.grey[400],
+      ),
+      suffixIconColor: Colors.black,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(
+          color: Color(0xFF56c1b1),
+          width: 0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(100),
+        borderSide: const BorderSide(
+          color: Color(0xFF56c1b1),
+          width: 1,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(100.0),
+        borderSide: const BorderSide(
+          color: Colors.red,
+          width: 1.0,
+        ),
+      ),
+      errorStyle: TextStyle(
+        backgroundColor: Colors.red.withOpacity(0.1),
+        fontSize: 12,
+        color: Colors.red,
+      ),
+      filled: true,
+      fillColor: const Color(0xFFF0F0F0),
+      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+    ),
+    cardColor: const Color(0x00000000),
+    dividerColor: Colors.black,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Color(0xFF42a79d),
+      foregroundColor: Colors.black,
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 
-  final lightTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.white,
-    brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-    // accentColor: Colors.black,
-    // accentIconTheme: IconThemeData(color: Colors.white),
-    dividerColor: Colors.white54,
+  final darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      primary: const Color(0xFF42a79d),
+      secondary: const Color(0xFF42a79d),
+      brightness: Brightness.dark,
+    ),
+    fontFamily: "GoogleSans",
+    primaryTextTheme: GoogleFonts.acmeTextTheme(),
+    backgroundColor: Colors.black,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      color: Color(0xFF42a79d),
+      foregroundColor: Color(0xFFF5F5F5),
+      iconTheme: IconThemeData(color: Color(0xFFF5F5F5)),
+    ),
+    bottomAppBarColor: Colors.black,
+    iconTheme: const IconThemeData(color: Color(0xFFF5F5F5)),
+    textTheme: const TextTheme(
+      headline1: TextStyle(
+        color: Color(0xFFF5F5F5),
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+      headline2: TextStyle(
+        color: Color(0xFFF5F5F5),
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      headline3: TextStyle(
+        color: Color(0xFFF5F5F5),
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyText1: TextStyle(
+        color: Color(0xFFF5F5F5),
+        fontSize: 16,
+      ),
+      bodyText2: TextStyle(
+        color: Color(0xFFF5F5F5),
+        fontSize: 16,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          const Color(0xFF42a79d),
+        ),
+        foregroundColor: MaterialStateProperty.all<Color>(
+          const Color(0xFFF5F5F5),
+        ),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: TextStyle(
+        color: Colors.grey[400],
+      ),
+      suffixIconColor: const Color(0xFFF5F5F5),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(
+          color: Color(0xFF56c1b1),
+          width: 0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(100),
+        borderSide: const BorderSide(
+          color: Color(0xFF56c1b1),
+          width: 1,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(100.0),
+        borderSide: const BorderSide(
+          color: Colors.red,
+          width: 1.0,
+        ),
+      ),
+      errorStyle: TextStyle(
+        backgroundColor: Colors.red.withOpacity(0.1),
+        fontSize: 12,
+        color: Colors.red,
+      ),
+      filled: true,
+      fillColor: Colors.black,
+      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+    ),
+    cardColor: const Color(0xFFF5F5F5),
+    dividerColor: const Color(0xFFF5F5F5),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Color(0xFF42a79d),
+      foregroundColor: Color(0xFFF5F5F5),
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 
   ThemeData _themeData = ThemeData();
