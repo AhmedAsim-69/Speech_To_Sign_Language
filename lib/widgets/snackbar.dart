6bc5caff8b08e04c35890c7afbd10dfdd25d91fb
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-14 18:52:02 +0500

    Modified Snackbar Style

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -3,49 +3,50 @@ import 'package:flutter/material.dart';
 class ShowSnackbar {
   static showsnackbar(Color bgColor, Color textColor, IconData prefixIcon,
       String text, BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
     return ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         backgroundColor: Colors.transparent,
         behavior: SnackBarBehavior.floating,
         elevation: 0,
         duration: const Duration(seconds: 3),
-        content: Container(
-          padding: const EdgeInsets.fromLTRB(20, 3, 5, 3),
-          height: height * 0.05,
-          width: width * 0.7,
-          decoration: BoxDecoration(
-            color: bgColor,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(30),
+        content: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(20, 3, 5, 3),
+            decoration: BoxDecoration(
+              color: bgColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(30),
+              ),
             ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                prefixIcon,
-                color: const Color(0xffffffff),
-                size: 14,
-              ),
-              Text(
-                text,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: textColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Icon(
+                    prefixIcon,
+                  ),
                 ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.close),
-                iconSize: 14,
-                color: const Color(0xffffffff),
-                onPressed: () {
-                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                },
-              ),
-            ],
+                Expanded(
+                  child: FittedBox(
+                    child: Text(
+                      text,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: textColor,
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
