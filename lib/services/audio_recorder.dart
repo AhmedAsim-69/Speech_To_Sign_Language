d90b292100d7720e19d19fc273c9c833382998b7
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-14 18:47:40 +0500

    Removed Unnecessary Functions

--- lib/services/audio_recorder.dart
+++ lib/services/audio_recorder.dart
@@ -1,29 +1,20 @@
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class AudioRecorder {
   static final recorder = FlutterSoundRecorder();
   static bool isRecorderReady = false;
+
   static Future startRecording() async {
     if (!isRecorderReady) return;
-    await recorder.startRecorder(
-        toFile:
-            '/storage/emulated/0/Android/data/com.example.stsl/files/audio.wav');
+    final path = (await (getExternalStorageDirectory()))!.path;
+    await recorder.startRecorder(toFile: '$path/audio.wav');
   }
 
   static Future stopRecording() async {
     if (!isRecorderReady) return;
     await recorder.stopRecorder();
-  }
-
-  static Future pauseRecording() async {
-    if (!isRecorderReady) return;
-    await recorder.pauseRecorder();
-  }
-
-  static Future resumeRecording() async {
-    if (!isRecorderReady) return;
-    await recorder.resumeRecorder();
   }
 
   static Future initRecorder() async {
@@ -35,7 +26,7 @@ class AudioRecorder {
       initRecorder();
     }
     await recorder.openRecorder();
-    isRecorderReady = true;
     recorder.setSubscriptionDuration(const Duration(milliseconds: 200));
+    isRecorderReady = true;
   }
 }
