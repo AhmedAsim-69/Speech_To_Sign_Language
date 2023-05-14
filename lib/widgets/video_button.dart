c2a8d1a3a298f8f558e69326dbb7c5a6f90aa87a
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-14 18:52:38 +0500

    Misc. Changes

--- lib/widgets/video_button.dart
+++ lib/widgets/video_button.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:stsl/pages/display_video.dart';
+import 'package:stsl/pages/pose_video.dart';
 
 class VideoButton extends StatelessWidget {
   const VideoButton({
     Key? key,
+    required this.text,
+    required this.isML,
   }) : super(key: key);
+
+  final String text;
+  final bool isML;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: Colors.blue[400],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+    return SizedBox(
+      width: 150,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(
+              width: 2,
+              color: Color(0xFF30847e),
+            ),
+          ),
         ),
-        minimumSize: const Size(80, 45),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    (isML) ? const MlVideo() : const DisplayVideo()),
+          );
+        },
+        child: Text(text),
       ),
-      onPressed: () {
-        // setState(() {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const DisplayVideo()),
-        );
-        // });
-      },
-      child: const Text('Play Video!'),
     );
   }
 }
