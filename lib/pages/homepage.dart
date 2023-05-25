55231a3e07a2c36262fafef1568748edea3ddf16
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-25 20:50:24 +0500

    Added Validations for Sign Request to API

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 
 import 'package:audio_waveforms/audio_waveforms.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
-
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
@@ -13,9 +12,10 @@ import 'package:stsl/functions/functions.dart';
 
 import 'package:stsl/services/api_call.dart';
 import 'package:stsl/services/audio_recorder.dart';
-import 'package:stsl/services/wave_bubble.dart';
+import 'package:stsl/services/pose_video_player.dart';
+import 'package:stsl/services/sign_video_player.dart';
 import 'package:stsl/services/user_simple_preferences.dart';
-import 'package:stsl/services/video_player.dart';
+import 'package:stsl/services/wave_bubble.dart';
 
 import 'package:stsl/utils/theme_data.dart';
 import 'package:stsl/widgets/snackbar.dart';
@@ -58,19 +58,22 @@ class _HomePageState extends State<HomePage> {
     AudioRecorder.initRecorder();
     MyFunctions.initStorage();
     ApiCall.wordsFound = UserSimplePreferences.getWords() ?? "";
+    ApiCall.poseWordsFound = UserSimplePreferences.getPoseWords() ?? "";
     ApiCall.wordsNotFound = UserSimplePreferences.getNotWords() ?? "";
-    LocalVideoPlayer.videoController();
+
+    SignVideoPlayer.videoController();
+    PoseVideoPlayer.videoController();
     _initialiseControllers();
   }
 
   @override
   void dispose() {
     AudioRecorder.recorder.closeRecorder();
-    if (LocalVideoPlayer.chewieController != null) {
-      LocalVideoPlayer.chewieController!.dispose();
+    if (SignVideoPlayer.signController != null) {
+      SignVideoPlayer.signController!.dispose();
     }
-    if (LocalVideoPlayer.chewieController1 != null) {
-      LocalVideoPlayer.chewieController1!.dispose();
+    if (PoseVideoPlayer.poseController != null) {
+      PoseVideoPlayer.poseController!.dispose();
     }
 
     super.dispose();
@@ -100,106 +103,119 @@ class _HomePageState extends State<HomePage> {
           title: const Text("Pakistan Sign Express"),
         ),
         drawer: Drawer(
-            child: ListView(
-          children: [
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Color(0xFF42a79d),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(
-                      'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg'),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.25,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Material(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(100.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          "assets/logos/app_logo.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        'Pakistan Sign Express',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text('Pakistan Sign Express')),
+              const Divider(),
+              SwitchListTile(
+                  title: const Text("Dark Mode"),
+                  value: _isDarkMode,
+                  onChanged: (value) {
+                    _isDarkMode = value;
+                    if (_isDarkMode) {
+                      theme.setDarkMode();
+                    } else {
+                      theme.setLightMode();
+                    }
+                  }),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.09,
               ),
-            ),
-            SwitchListTile(
-                title: const Text("Dark Mode"),
-                value: _isDarkMode,
-                onChanged: (value) {
-                  _isDarkMode = value;
-                  if (_isDarkMode) {
-                    theme.setDarkMode();
-                  } else {
-                    theme.setLightMode();
-                  }
-                }),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 80,
-                  width: 80,
-                  child: Image.asset(
-                    'assets/logos/uet_logo.png',
-                    fit: BoxFit.contain,
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: Image.asset(
+                      'assets/logos/uet_logo.png',
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-              child: Text(
-                "University of Engineering and Technology, Lahore",
+              const Padding(
+                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: Text(
+                  "University of Engineering and Technology, Lahore",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Divider(),
+              const Text(
+                "2019-FYP-13",
                 textAlign: TextAlign.center,
               ),
-            ),
-            const Divider(),
-            const Text(
-              "2019-FYP-13",
-              textAlign: TextAlign.center,
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(15, 5, 15, 0),
-              child: Text(
-                "Real-time Speech-to-Pakistan Sign Language using Neural Networks",
+              const Padding(
+                padding: EdgeInsets.fromLTRB(15, 5, 15, 0),
+                child: Text(
+                  "Real-time Speech-to-Pakistan Sign Language using Neural Networks",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Divider(),
+              const Text(
+                "Supervised By:",
                 textAlign: TextAlign.center,
               ),
-            ),
-            const Divider(),
-            const Text(
-              "Supervised By:",
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "Prof. Dr. Kashif Javed",
-              textAlign: TextAlign.center,
-            ),
-            const Divider(),
-            const Text(
-              "2019-EE-31: Ahmed Asim",
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "2019-EE-27: Muhammad Ashar Khan",
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "2019-EE-51: Aimon Humayun",
-              textAlign: TextAlign.center,
-            ),
-            const Divider(),
-            const Text(
-              "App Developer: Ahmed Asim",
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "Email: ahmedk082@gmail.com ",
-              textAlign: TextAlign.center,
-            ),
-            const Divider(),
-          ],
-        )),
+              const Text(
+                "Prof. Dr. Kashif Javed",
+                textAlign: TextAlign.center,
+              ),
+              const Divider(),
+              const Text(
+                "2019-EE-31: Ahmed Asim",
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "2019-EE-27: Muhammad Ashar Khan",
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "2019-EE-51: Aimon Humayun",
+                textAlign: TextAlign.center,
+              ),
+              const Divider(),
+              const Text(
+                "App Developer: Ahmed Asim",
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "Email: ahmedk082@gmail.com ",
+                textAlign: TextAlign.center,
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
         body: ModalProgressHUD(
           opacity: 0.8,
           blur: 1.0,
@@ -227,27 +243,33 @@ class _HomePageState extends State<HomePage> {
                 Padding(
                   padding: const EdgeInsets.only(top: 15.0),
                   child: WordsContainer(
-                      text: ApiCall.wordsFound,
-                      altText: "No Sign Language",
-                      poseText:
-                          "Sign Language found for the following words: "),
+                    text: ApiCall.wordsFound,
+                    altText: "No Sign Language",
+                    poseText: "Sign Language found for the following words: ",
+                  ),
                 ),
                 SizedBox(
                   height: sizedBoxHeight,
                 ),
                 WordsContainer(
-                    text: ApiCall.wordsNotFound,
-                    altText: "No Sign Language",
-                    poseText:
-                        "Sign Language Not Found for the following words: "),
+                  text: ApiCall.wordsNotFound,
+                  altText: "No Sign Language",
+                  poseText: "Sign Language Not Found for the following words: ",
+                ),
                 SizedBox(
                   height: sizedBoxHeight,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: const [
-                    VideoButton(text: "Play Human Video", isML: false),
-                    VideoButton(text: "Play Pose Video", isML: true),
+                    VideoButton(
+                      text: "Play Human Video",
+                      isML: false,
+                    ),
+                    VideoButton(
+                      text: "Play Pose Video",
+                      isML: true,
+                    ),
                   ],
                 ),
                 SizedBox(
@@ -274,19 +296,28 @@ class _HomePageState extends State<HomePage> {
                       ),
                       IconButton(
                         onPressed: () async {
-                          final path =
-                              (await getExternalStorageDirectory())!.path;
-                          if (mounted) {
-                            File audio = File('$path/audio.wav');
-                            (audio.existsSync())
-                                ? await ApiCall.uploadSpeech(audio, null, true,
-                                    false, false, _updateState, context)
-                                : ShowSnackbar.showsnackbar(
-                                    Colors.black54,
-                                    Colors.yellow,
-                                    Icons.warning,
-                                    "No Audio Recorded",
-                                    context);
+                          if (!isRec) {
+                            final path =
+                                (await getExternalStorageDirectory())!.path;
+                            if (mounted) {
+                              File audio = File('$path/audio.wav');
+                              (audio.existsSync())
+                                  ? await ApiCall.uploadSpeech(audio, null,
+                                      true, false, false, _updateState, context)
+                                  : ShowSnackbar.showsnackbar(
+                                      Colors.black54,
+                                      Colors.yellow,
+                                      Icons.warning,
+                                      "No Audio Recorded",
+                                      context);
+                            }
+                          } else {
+                            ShowSnackbar.showsnackbar(
+                                Colors.black54,
+                                Colors.yellow,
+                                Icons.warning,
+                                "Audio is being recorded",
+                                context);
                           }
                         },
                         icon: const Icon(
@@ -406,31 +437,46 @@ class _HomePageState extends State<HomePage> {
                       FloatingActionButton.small(
                         heroTag: "pose video",
                         onPressed: () async {
-                          if (_textEditingController.text.isNotEmpty) {
-                            _isFormValid = true;
-                            await ApiCall.uploadSpeech(
-                                null,
-                                _textEditingController.text,
-                                false,
-                                true,
-                                true,
-                                _updateState,
-                                context);
-                          } else {
-                            final path =
-                                (await getExternalStorageDirectory())!.path;
-                            if (mounted) {
-                              File audio = File('$path/audio.wav');
-                              (audio.existsSync())
-                                  ? await ApiCall.uploadSpeech(audio, null,
-                                      true, false, true, _updateState, context)
-                                  : ShowSnackbar.showsnackbar(
-                                      Colors.black54,
-                                      Colors.yellow,
-                                      Icons.warning,
-                                      "No Audio Recorded",
-                                      context);
+                          if (!isRec) {
+                            if (_textEditingController.text.isNotEmpty) {
+                              _isFormValid = true;
+                              await ApiCall.uploadSpeech(
+                                  null,
+                                  _textEditingController.text,
+                                  false,
+                                  true,
+                                  true,
+                                  _updateState,
+                                  context);
+                            } else {
+                              final path =
+                                  (await getExternalStorageDirectory())!.path;
+                              if (mounted) {
+                                File audio = File('$path/audio.wav');
+                                (audio.existsSync())
+                                    ? await ApiCall.uploadSpeech(
+                                        audio,
+                                        null,
+                                        true,
+                                        false,
+                                        true,
+                                        _updateState,
+                                        context)
+                                    : ShowSnackbar.showsnackbar(
+                                        Colors.black54,
+                                        Colors.yellow,
+                                        Icons.warning,
+                                        "No Audio Recorded",
+                                        context);
+                              }
                             }
+                          } else {
+                            ShowSnackbar.showsnackbar(
+                                Colors.black54,
+                                Colors.yellow,
+                                Icons.warning,
+                                "Audio is being recorded",
+                                context);
                           }
                           setState(() {});
                         },
