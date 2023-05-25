f77db8b8fa9073395c5e0e82cc54790fe87bbb6f
AhmedAsim-69 <ahmedk082@gmail.com>
2023-05-25 20:57:45 +0500

    Fixed Permission Exception

--- lib/services/get_permission.dart
+++ lib/services/get_permission.dart
@@ -4,38 +4,89 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:stsl/pages/homepage.dart';
-
 import 'package:stsl/widgets/snackbar.dart';
 
 class GetPermission {
-  static void goToHome(context) async {
-    Map<Permission, PermissionStatus> status = await [
-      Permission.audio,
-      Permission.microphone,
-      Permission.storage,
-    ].request();
-
-    if (status.toString().contains('PermissionStatus.granted')) {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      await preferences.setInt('isBoarding', 0);
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomePage()),
-      );
-    } else {
-      ShowSnackbar.showsnackbar(
+  static late PermissionStatus microphoneStatus;
+  static late PermissionStatus storageStatus;
+  static Future<void> goToHome(context) async {
+    microphoneStatus = await Permission.microphone.request();
+    if (microphoneStatus.isGranted) {
+      storageStatus = await Permission.storage.request();
+      if (storageStatus.isGranted) {
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        await preferences.setInt('isBoarding', 0);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      } else if (storageStatus.isDenied || storageStatus.isRestricted) {
+        storageStatus = await Permission.storage.request();
+        if (storageStatus.isGranted) {
+          SharedPreferences preferences = await SharedPreferences.getInstance();
+          await preferences.setInt('isBoarding', 0);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        } else {
+          ShowSnackbar.showsnackbar(
+            Colors.black54,
+            Colors.orange,
+            Icons.warning,
+            'Please enable storage permission from Settings',
+            context,
+          );
+          openAppSettings();
+        }
+      } else if (storageStatus.isPermanentlyDenied) {
+        ShowSnackbar.showsnackbar(
           Colors.black54,
           Colors.orange,
           Icons.warning,
-          '''Please re-open app and/or 
-          Enable Permissions from Settings''',
-          context);
-    }
-    if (status.toString().contains('PermissionStatus.permanentlyDenied') ||
-        status.toString().contains('PermissionStatus.restricted')) {
-      ShowSnackbar.showsnackbar(Colors.black54, Colors.orange, Icons.warning,
-          "Please enable required Permissions", context);
-
+          'Please allow storage permission from Settings',
+          context,
+        );
+        openAppSettings();
+      }
+    } else if (microphoneStatus.isDenied || microphoneStatus.isRestricted) {
+      microphoneStatus = await Permission.microphone.request();
+      if (microphoneStatus.isGranted) {
+        storageStatus = await Permission.storage.request();
+        if (storageStatus.isGranted) {
+          SharedPreferences preferences = await SharedPreferences.getInstance();
+          await preferences.setInt('isBoarding', 0);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        } else {
+          ShowSnackbar.showsnackbar(
+            Colors.black54,
+            Colors.orange,
+            Icons.warning,
+            'Please enable storage permission from Settings',
+            context,
+          );
+          openAppSettings();
+        }
+      } else {
+        ShowSnackbar.showsnackbar(
+          Colors.black54,
+          Colors.orange,
+          Icons.warning,
+          'Please enable microphone permission from Settings',
+          context,
+        );
+      }
+    } else if (microphoneStatus.isPermanentlyDenied) {
+      ShowSnackbar.showsnackbar(
+        Colors.black54,
+        Colors.orange,
+        Icons.warning,
+        'Please allow microphone permission from Settings',
+        context,
+      );
       openAppSettings();
     }
   }
